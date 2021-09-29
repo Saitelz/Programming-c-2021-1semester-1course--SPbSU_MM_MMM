8fdb009741092e5356ad80106f3399edd167e3bf
Saitelz <saitelz@yandex.ru>
2021-09-29 20:09:55 +0300

    Task2 from 2021.09.26-Homework-2 upload

--- 2021.09.26-Homework-2/Task2/main.cpp
+++ 2021.09.26-Homework-2/Task2/main.cpp
@@ -4,7 +4,23 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    cout << "Hello, World!" << endl;
+    int n = 0;
+
+    cin >> n;
+
+    if ((n % 4) == 0 && (n % 100) != 0)
+    {
+        cout << "YES";
+    }
+    else
+        if ((n % 400) == 0)
+        {
+            cout << "YES";
+        }
+        else
+        {
+            cout << "NO";
+        }
 
     return EXIT_SUCCESS;
 }
