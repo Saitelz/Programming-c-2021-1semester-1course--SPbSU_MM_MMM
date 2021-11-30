68d852cd4d8f8b3d6a7de88ce8ea25564e180ba3
Saitelz <saitelz@yandex.ru>
2021-11-30 19:50:48 +0300

    Task6 style corrections and Task7 from 2021.09.26-Homework-2 upload

--- 2021.09.26-Homework-2/Task6/main.cpp
+++ 2021.09.26-Homework-2/Task6/main.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    int CorQ1 = 0;
-    int CorQ2 = 0;
-    int CorF1 = 0;
-    int CorF2 = 0;
+    int x1 = 0;
+    int y1 = 0;
+    int x2 = 0;
+    int y2 = 0;
 
-    cin >> CorQ1 >> CorQ2 >> CorF1 >> CorF2;
+    cin >> x1 >> y1 >> x2 >> y2;
 
-    if ((CorQ1 == CorF1) || (CorQ2 == CorF2) || (abs(CorQ1 - CorF1) == abs(CorQ2 - CorF2)))
+    if ((x1 == x2) || (y1 == y2) || (abs(x1 - x2) == abs(y1 - y2)))
     {
         cout << "YES";
     }
